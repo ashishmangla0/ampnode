6847bc800b88323dddae7e1e9676f87c33f0d0db
ashish <ashish@skytesters.com>
2020-01-20 19:22:54 +0530

    a

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const app = express()
+const port = 3000
+app.get('/', (req, res) => res.send('Hello World!'))
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+nunjucks.configure('views', {
+    autoescape: true,
+    express: app
+});
+app.get('/', function(req, res) {
+    res.render('index.html');
+});
